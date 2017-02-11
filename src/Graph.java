9428f0ce2e869226c15c6687a38e9098ec86e37f
Anders Nylund <anders.nylund.an@gmail.com>
2017-02-11 16:06:09 +0200

    Implement with HashMap

--- src/Graph.java
+++ src/Graph.java
@@ -1,27 +1,81 @@
 import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 public class Graph {
 
-  private Dictionary<Integer, ArrayList<Integer>> nodes = new Hashtable<>();
+  private Map<Integer, List<Integer>> arcs = new HashMap<>();
+  private Random rand = new Random();
+
+  public Graph() {}
+
+  public Graph(int n) {
+    for(int i = 0; i < n; i++) {
+      for(int j = 0; j < rand.nextInt(n); j++) {
+        addArc(i+1, rand.nextInt(n)+1);
+      }
+    }
+
+    for(Map.Entry<Integer, List<Integer>> entry : arcs.entrySet()) {
+      entry.getValue().sort(Integer::compareTo);
+    }
+
+  }
 
   public void addArc(int startIndex, int endIndex) {
-    ArrayList<Integer> endNodes = nodes.get(startIndex);
+    List<Integer> endNodes = arcs.get(startIndex);
     if(endNodes != null) {
       if(endNodes.contains(endIndex))
         return;
       endNodes.add(endIndex);
     }
     else {
-      if (nodes.get(endIndex) != null) {
-        if (nodes.get(endIndex).contains(startIndex))
+      if (arcs.get(endIndex) != null) {
+        if (arcs.get(endIndex).contains(startIndex))
           return;
       }
       endNodes = new ArrayList<>();
       endNodes.add(endIndex);
     }
-    nodes.put(startIndex, endNodes);
+    arcs.put(startIndex, endNodes);
+  }
+
+  public void print() {
+    for(Map.Entry<Integer,List<Integer>> entry : arcs.entrySet()) {
+      System.out.print(entry.getKey() + " -> ");
+      for(Integer value : entry.getValue()) {
+        System.out.print(value + " ");
+      }
+      System.out.println();
+    }
+  }
+
+  public boolean infPath(int startNode) {
+    return findEndNodesRecursive(startNode, new ArrayList<>());
+  }
+
+  private boolean findEndNodesRecursive (int startNode, List<Integer> checkedNodes) {
+    if (checkedNodes.contains(startNode)) {
+      return true;
+    }
+
+    List<Integer> copiedNodes = new ArrayList<>();
+    for(Integer node : checkedNodes) {
+      copiedNodes.add(node);
+    }
+
+    copiedNodes.add(startNode);
+
+    if (arcs.containsKey(startNode)) {
+      for(Integer endNode : arcs.get(startNode)) {
+        if(findEndNodesRecursive(endNode, copiedNodes)) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,48 @@
 public class Main {
 
   public static void main(String[] args) {
-    Graph graph4 = new Graph();
-    graph4.addArc(1, 2);
-    graph4.addArc(2, 3);
-    graph4.addArc(2, 4);
-    graph4.addArc(3, 5);
-    graph4.addArc(4, 5);
-    graph4.addArc(5, 2);
+
+    Graph graph = new Graph(1000);
+    graph.print();
+
+    System.out.print(graph.infPath(1));
+
   }
 }
+
+//    Graph graph4 = new Graph();
+//    graph4.addArc(1, 2);
+//    graph4.addArc(2, 3);
+//    graph4.addArc(2, 4);
+//    graph4.addArc(3, 5);
+//    graph4.addArc(4, 5);
+//    graph4.addArc(5, 2);
+//
+//    graph4.print();
+//
+//    System.out.println(graph4.infPath(1));
+
+
+//    Graph graph1 = new Graph();
+//    graph1.addArc(1, 2);
+//    graph1.addArc(1, 4);
+//    graph1.addArc(2, 3);
+//    graph1.addArc(2, 5);
+//    graph1.addArc(3, 6);
+//    graph1.addArc(4, 5);
+//    graph1.addArc(4, 7);
+//    graph1.addArc(5, 1);
+//    graph1.addArc(5, 3);
+//    graph1.addArc(5, 6);
+//    graph1.addArc(5, 8);
+//    graph1.addArc(5, 9);
+//    graph1.addArc(6, 9);
+//    graph1.addArc(7, 5);
+//    graph1.addArc(7, 8);
+//
+//
+//    graph1.addArc(8, 9);
+//
+//    graph1.print();
+//
+//    System.out.print(graph1.infPath(6));
